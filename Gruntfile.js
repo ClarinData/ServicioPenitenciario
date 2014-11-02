b5d4066ba287db12fb41d078315e4ff36c6a4ab8
Juan Pablo Kutianski <jkutianski@gmail.com>
2014-11-02 07:29:27 -0300

    Grunt

---
+++ Gruntfile.js
@@ -0,0 +1,43 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+      },
+      all: {
+        files: [{
+            expand: true,
+            cwd: 'js/',
+            src: ['*.js', '!*.min.js'],
+            dest: 'js/',
+            ext: '.min.js'
+        }]
+      }
+    },
+    cssmin: {
+      options: {
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+      },
+      all: {
+        expand: true,
+        cwd: 'css/',
+        src: ['*.css', '!*.min.css'],
+        dest: 'css/',
+        ext: '.min.css'
+      }
+    }
+  });
+
+  // Load the plugin that provides the "uglify" task.
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+
+  // Load the plugin that provides the "CSS minify" task.
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+
+  // Default task(s).
+  grunt.registerTask('default', ['cssmin','uglify']);
+
+};
